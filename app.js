737d0df664d59e29caafbd1171116cadd363ffed
Ramesh Raveendran <rameshraveendranm@gmail.com>
2025-12-24 22:44:29 +0530

    server check

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app = express();
+
+
+app.get('/', (req , res) => {
+    res.end('this server is running at 3005')
+})
+
+
+app.listen(3005 , () => {
+    console.log('Server is running at localhost 3005')
+});
