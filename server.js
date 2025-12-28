ee7716d1a47910857849c2e9c2ed999e38cadc94
Ramesh Raveendran <rameshraveendranm@gmail.com>
2025-12-28 21:29:35 +0530

    mongodb check

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+
+require('dotenv').config();
 //instance 
 const express = require('express');
+const path = require ('path');
+const connectDB = require("./config/db")
 // server app
 const app = express();
+
 
 //route
 app.get('/',(req , res) => {
     res.send('server is live')
-})
+});
 
-
+// db calling allways add top of the server
+connectDB();
 //server started
 app.listen(3003 , () => {
     console.log('server started at 3003');
