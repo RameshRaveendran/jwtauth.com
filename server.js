6abd260a9c25d66c5c29ac3c23f1e8a698941610
Ramesh Raveendran <rameshraveendranm@gmail.com>
2025-12-29 16:30:39 +0530

    jwt checks

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-
 require('dotenv').config();
-//instance 
 const express = require('express');
-const path = require ('path');
-const connectDB = require("./config/db")
-// server app
-const app = express();
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
-
-//route
-app.get('/',(req , res) => {
-    res.send('server is live')
-});
-app.use(express.json());
-// 🔴 THIS LINE IS MANDATORY
-app.use("/auth", require("./routes/auth.routes"));
-
-// db calling allways add top of the server
 connectDB();
-//server started
-app.listen(3003 , () => {
-    console.log('server started at 3003');
-});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
